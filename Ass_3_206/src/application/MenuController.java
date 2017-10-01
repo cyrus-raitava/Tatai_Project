22c9e2b22673a9abbf724435737c15bbc50801db
Tr0iBo1abLe <32250260+Tr0iBo1abLe@users.noreply.github.com>
2017-10-01 13:17:44 +1300

    Update MenuController.java

--- Ass_3_206/src/application/MenuController.java
+++ Ass_3_206/src/application/MenuController.java
@@ -8,6 +8,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+/**
+ * @author t-mcl
+ *
+ */
 public class MenuController {
 
 	
@@ -17,18 +21,34 @@ public class MenuController {
 	@FXML
 	private Button howToButton;
 	
+	/**
+	 * When timata is pressed, change scenes to level menu
+	 * @param event
+	 * @throws IOException
+	 */
 	public void timataPress(ActionEvent event) throws IOException {
 		Scene levelMenu = SceneStorage.getInstance().levelMenu;
 		Stage window = (Stage) timataButton.getScene().getWindow();
 		window.setScene(levelMenu);
 	}
 	
+	
+	/**
+	 * When how to play button is pressed, switch to instructions scene.
+	 * @param event
+	 * @throws IOException
+	 */
 	public void howTo(ActionEvent event) throws IOException {
 		Scene instructions = SceneStorage.getInstance().instructions;
 		Stage window = (Stage) timataButton.getScene().getWindow();
 		window.setScene(instructions);
 	}
 	
+	/**
+	 * When stats button is pressed, switch to statistics scene.
+	 * @param event
+	 * @throws IOException
+	 */
 	public void statsPress(ActionEvent event) throws IOException {
 		Scene statistics = SceneStorage.getInstance().statistics;
 		Stage window = (Stage) timataButton.getScene().getWindow();
