3d5df32c798084b0e8f520436f20d29e7a8b30c0
cyrus-raitava <31422519+cyrus-raitava@users.noreply.github.com>
2017-10-02 00:47:48 +1300

    Added Instructions description, and necessary aesthetic layout.

--- Ass_3_206/src/application/InstructionsController.java
+++ Ass_3_206/src/application/InstructionsController.java
@@ -1,16 +1,33 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-public class InstructionsController {
+public class InstructionsController implements Initializable {
 
 	
+	@FXML
+	public TextArea dataPane = new TextArea();
+	
+	@FXML
+	public Label disclaimer;
+	
+
 	@FXML
 	private Button returnButton;
 	
@@ -23,5 +40,25 @@ public class InstructionsController {
 		Scene menu = SceneStorage.getInstance().menu;
 		Stage window = (Stage) returnButton.getScene().getWindow();
 		window.setScene(menu);
+	}
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		disclaimer.setVisible(true);
+		
+		
+		dataPane.setMaxWidth(540);
+		dataPane.setEditable(false);
+		dataPane.setWrapText(true);
+		dataPane.setText("Tēnā Koe! Our game Tātai will teach you how to pronounce Māori numbers!");
+		dataPane.appendText("\n");
+		dataPane.appendText("\nFirstly, pick the level you want to play on: the Easy level will test you on numbers 1 to 9, and the Hard level will test you on numbers 1 to 99.");
+		dataPane.appendText("\n\nAfter choosing your level, the game will display a number to you: try to pronounce it in Māori!");
+		dataPane.appendText("\n\nIf you are right, you will get a point. After 10 questions, you will get your score, which you can view -with all your other session scores- on the Statistics option!");
+		dataPane.appendText("\n\nAll the best, and Waimarie Pai! (Good Luck!)");
+		dataPane.selectPositionCaret(0);
+		dataPane.setVisible(true);
+	
 	}
 }
