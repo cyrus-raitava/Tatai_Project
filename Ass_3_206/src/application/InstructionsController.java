b3621503e281c70e9a829499c04bf39a11bbd675
Tr0iBo1abLe <32250260+Tr0iBo1abLe@users.noreply.github.com>
2017-10-01 14:23:44 +1300

    Anti fucking up the fuck up

--- Ass_3_206/src/application/InstructionsController.java
+++ Ass_3_206/src/application/InstructionsController.java
@@ -8,62 +8,20 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class LevelMenuController {
+public class InstructionsController {
+
 	
 	@FXML
-	private Button easyGoButton;
-	
-	@FXML
-	private Button hardGoButton;
-	
-	@FXML
-	private Button mainMenuButton;
+	private Button returnButton;
 	
 	/**
-	 * When 'Go!' button for easy is pressed, move to quiz scene with hard set to 
-	 * false.
+	 * Switches to main menu scene when return button is pressed
 	 * @param event
 	 * @throws IOException
 	 */
-	public void easyGoPress(ActionEvent event) throws IOException {
-		SceneStorage.getInstance().quizSetup(); // reset quiz to initial state
-		SceneStorage.getInstance().qc.hard = false; // set hard to false
-		
-		// swap scenes
-		Scene quiz = SceneStorage.getInstance().quiz;
-		Stage window = (Stage) easyGoButton.getScene().getWindow();
-		window.setScene(quiz);
-	}
-	
-	
-	
-	/**
-	 * When 'Go!' button for hard is pressed, move to quiz scene with hard set to 
-	 * true.
-	 * @param event
-	 * @throws IOException
-	 */
-	public void hardGoPress(ActionEvent event) throws IOException {
-		SceneStorage.getInstance().quizSetup(); // reset quiz to initial state
-		SceneStorage.getInstance().qc.hard = true; // set hard to true
-		
-		// swap scenes
-		Scene quiz = SceneStorage.getInstance().quiz;
-		Stage window = (Stage) hardGoButton.getScene().getWindow();
-		window.setScene(quiz);
-	}
-	
-	
-	/**
-	 * When MainMenu is pressed, switch to menu scene.
-	 * @param event
-	 * @throws IOException
-	 */
-	public void mainMenuPress(ActionEvent event) throws IOException {
+	public void returnPress(ActionEvent event) throws IOException {
 		Scene menu = SceneStorage.getInstance().menu;
-		Stage window = (Stage) hardGoButton.getScene().getWindow();
+		Stage window = (Stage) returnButton.getScene().getWindow();
 		window.setScene(menu);
 	}
-	
-
 }
